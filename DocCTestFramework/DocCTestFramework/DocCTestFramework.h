6f257f5779fb7d6577e6189832608bcba1436047
Yoon Jinyeong <wwwz4805@kakao.com>
2021-06-14 09:07:34 +0900

    Create DocCTestFramework Project

---
+++ DocCTestFramework/DocCTestFramework/DocCTestFramework.h
@@ -0,0 +1,18 @@
+//
+//  DocCTestFramework.h
+//  DocCTestFramework
+//
+//  Created by 윤진영 on 2021/06/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DocCTestFramework.
+FOUNDATION_EXPORT double DocCTestFrameworkVersionNumber;
+
+//! Project version string for DocCTestFramework.
+FOUNDATION_EXPORT const unsigned char DocCTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DocCTestFramework/PublicHeader.h>
+
+
